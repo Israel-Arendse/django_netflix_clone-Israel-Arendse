d78f8e35304c66063f93645967e61b2963c9433d
codemason43 <israelarendsefortnite@gmail.com>
2025-08-30 15:08:52 +0400

    Step 3: Create Index Page

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import Home  # import Home view
 
 app_name = "core"
 
-urlpatterns = []
+# set url path for Home page
+urlpatterns = [path("", Home.as_view())]
