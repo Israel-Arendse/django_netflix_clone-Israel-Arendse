b4b1061bfd93c886d0af679934745c950011706e
codemason43 <israelarendsefortnite@gmail.com>
2025-09-03 19:51:48 +0400

    Step 13: Create Profile View

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import Home  # import Home view
+from .views import Home, ProfileList  # import Home view
 
 app_name = "core"
 
 # set url path for Home page
-urlpatterns = [path("", Home.as_view())]
+urlpatterns = [
+    path("", Home.as_view()),
+    # URL path for ProfileList
+    path("profile/", ProfileList.as_view(), name="profile_list"),
+]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 
+# Import login_requred and method_decorator
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
+
+# Home view
 class Home(View):  # class for inheriting 'home' view
     def get(self, request, *args, **kwargs):
         return render(request, "index.html")
+
+
+# Profile view
+@method_decorator(login_required, name="dispatch")
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        return render(request, "profileList.html", {"profiles": profiles})
